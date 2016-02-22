0fa0a68c53386b68922badb47999bfc26007dc6b
Alice Aponasko <alice.aponasko@gmail.com>
2016-02-21 21:03:09 -0500

    Add peek and pop

---
+++ PeekAndPop/MainPhotoCell.swift
@@ -0,0 +1,17 @@
+//
+//  MainPhotoCell.swift
+//  PeekAndPop
+//
+//  Created by Alice Aponasko on 2/21/16.
+//  Copyright © 2016 aliceaponasko. All rights reserved.
+//
+
+import UIKit
+
+let MainPhotoCellID = "MainPhotoCell"
+
+class MainPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
